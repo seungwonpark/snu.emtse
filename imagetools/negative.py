cc4725532479954a1b9733d1cd9ea87ea7c6fdf1
seungwon park <yyyyy@snu.ac.kr>
2018-12-01 13:04:43 +0900

    week 7 plots

--- imagetools/negative.py
+++ imagetools/negative.py
@@ -3,9 +3,9 @@ import PIL.ImageOps
 import os
 
 for img in os.listdir('source/'):
-	print('Processing %s' % img)
-	if img[-3:] != 'PNG':
+	if img[-3:].lower() != 'png':
 		continue
+	print('Processing %s' % img)
 	image = Image.open('source/%s' % img)
 	inverted_image = PIL.ImageOps.invert(image)
 	inverted_image.save('result/neg-%s' % img)
